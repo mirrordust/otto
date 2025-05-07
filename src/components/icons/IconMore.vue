9a5c31320bc28cf97781b5b06db5c0e45ea819a9
mirrordust <gsmr19re@outlook.com>
2025-05-07 16:00:19 +0800

    update icons

--- src/components/icons/IconMore.vue
+++ src/components/icons/IconMore.vue
@@ -1,10 +1,13 @@
 <template>
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g clip-path="url(#clip0_4679_30)">
-      <path d="M16.59 8.59003L12 13.17L7.41 8.59003L6 10L12 16L18 10L16.59 8.59003Z" fill="white" />
+    <g clip-path="url(#clip0_5140_120)">
+      <path
+        d="M16.59 8.58984L12 13.1698L7.41 8.58984L6 9.99984L12 15.9998L18 9.99984L16.59 8.58984Z"
+        fill="#9100FF"
+      />
     </g>
     <defs>
-      <clipPath id="clip0_4679_30">
+      <clipPath id="clip0_5140_120">
         <rect width="24" height="24" fill="white" />
       </clipPath>
     </defs>
