b7954f160f13e200c955f214ff0ca9784b9eea08
mirrordust <gsmr19re@outlook.com>
2025-05-15 00:01:54 +0800

    更新人员信息

--- src/api/data.ts
+++ src/api/data.ts
@@ -4,7 +4,7 @@ const members = [
     id: 1,
     photo: '/images/photo/mengqian.png',
     name: 'Mengqian Lu',
-    intro: 'Earth Engineering',
+    intro: 'Director of CCRS, Earth Engineering',
     homePage: 'https://seng.hkust.edu.hk/about/people/faculty/mengqian-lu',
   },
   {
