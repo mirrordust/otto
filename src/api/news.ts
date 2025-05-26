835f2614531963f586b293dbffb07d95998b5c1b
mirrordust <gsmr19re@outlook.com>
2025-05-26 22:03:11 +0800

    公众号链接🔗

---
+++ src/api/news.ts
@@ -0,0 +1,55 @@
+const news = [
+  {
+    id: 1,
+    title:
+      '重磅 | 港科大陆萌茜教授牵头的“无缝预测与服务”计划获联合国教科文组织认可，入选“科学促进可持续发展国际十年”首批认可计划清单',
+    date: '2025-05-22',
+    link: 'https://mp.weixin.qq.com/s/kpH95DGjtyeYrEsCnU7eKA',
+  },
+  {
+    id: 2,
+    title: '香港科技大学陆萌茜教授荣获第50届日内瓦国际发明展金奖',
+    date: '2025-04-13',
+    link: 'https://mp.weixin.qq.com/s/KwWawxzWiaJUjQXh6BVKmQ',
+  },
+  {
+    id: 3,
+    title: '香港科技大学潘樂陶气候变化与可持续发展研究中心正式成立：共同应对气候变化挑战',
+    date: '2025-04-02',
+    link: 'https://mp.weixin.qq.com/s/QwC9N30ihmmBMrAVSph2Bg',
+  },
+  {
+    id: 4,
+    title: 'Journal of Climate | 青藏高原南部加热对北太平洋大气河远距离影响',
+    date: '2025-02-22',
+    link: 'https://mp.weixin.qq.com/s/O9x2GePCZL2wRSftB6-X7g',
+  },
+  {
+    id: 5,
+    title: 'DeepSeek领跑AtmosSci-Bench评测|大语言模型在大气科学中的最新进展',
+    date: '2025-02-01',
+    link: 'https://mp.weixin.qq.com/s/SajIVnftkUSgWNOdtuDRAQ',
+  },
+  {
+    id: 6,
+    title: '伴随港科大上海中心成立，数字气象服务创新实验室正式启航',
+    date: '2025-01-28',
+    link: 'https://mp.weixin.qq.com/s/0Oh2Pw-dYqwP465M16P91w',
+  },
+  {
+    id: 7,
+    title: '重要通知 | 第五届港科大“Climate, Weather, Water Forum" 开放注册！',
+    date: '2025-01-20',
+    link: 'https://mp.weixin.qq.com/s/jpOKWeihKYD28ZiPDjdpvQ',
+  },
+  {
+    id: 8,
+    title: 'Journal of Hydrology | 潜流交换对河流甲烷含量的动态变化和排放通量的关键作用',
+    date: '2024-11-26',
+    link: 'https://mp.weixin.qq.com/s/gB5Crqm4lHeuGzM5tUkAsw',
+  },
+]
+
+export const getNews = () => {
+  return news
+}
--- src/components/NewsSection.vue
+++ src/components/NewsSection.vue
@@ -1,7 +1,56 @@
+<script setup lang="ts">
+import { computed, ref, onMounted, onBeforeUnmount } from 'vue'
+import { getNews } from '@/api/news'
+import dayjs from 'dayjs'
+
+let timer: number = -1
+const newsList = ref(getNews())
+const numPerPage = ref(4)
+const startIdx = ref(0)
+
+const displayNewsList = computed(() => {
+  return newsList.value.slice(startIdx.value, startIdx.value + numPerPage.value)
+})
+const totalPages = computed(() => {
+  return Math.ceil(newsList.value.length / numPerPage.value)
+})
+const currentPage = computed(() => {
+  return Math.floor(startIdx.value / numPerPage.value) + 1
+})
+function gotoPage(n: number) {
+  clearInterval(timer)
+
+  startIdx.value = (n - 1) * numPerPage.value
+
+  startTimer()
+}
+function gotoNextPage() {
+  let nextPage = currentPage.value + 1
+  if (nextPage > totalPages.value) {
+    nextPage = 1
+  }
+  gotoPage(nextPage)
+}
+
+function dateTransform(date: string) {
+  const originalDate = dayjs(date)
+  return originalDate.format('MMM DD, YYYY')
+}
+
+const startTimer = () => {
+  timer = setInterval(() => {
+    gotoNextPage()
+  }, 20000) // 20秒切换一次
+}
+onMounted(startTimer)
+onBeforeUnmount(() => clearInterval(timer))
+</script>
+
 <template>
   <div class="content-container">
-    <h3>News</h3>
+    <h3>News&Events</h3>
 
+    <!-- events -->
     <div class="grid">
       <div class="desc">
         <div class="desc-content">
@@ -16,10 +65,39 @@
         <img src="/images/news/first_meeting.jpg" class="cover" />
       </div>
     </div>
+
+    <!-- links-news -->
+    <div class="links-news-container">
+      <a
+        class="link-news"
+        v-for="linkNews in displayNewsList"
+        :key="linkNews.id"
+        :href="linkNews.link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div class="date">{{ dateTransform(linkNews.date) }}</div>
+        <div class="title">{{ linkNews.title }}</div>
+      </a>
+
+      <div class="links-news-nav">
+        <div
+          v-for="n in totalPages"
+          :key="n"
+          class="circle"
+          :class="{ selected: currentPage == n }"
+          @click="gotoPage(n)"
+        ></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.content-container {
+  padding-bottom: 80px;
+}
+
 h3 {
   font-family: 'Noto Sans';
   font-weight: 500;
@@ -31,6 +109,10 @@ h3 {
   padding-top: 80px;
   padding-left: 100px;
 }
+
+/* ----------------------------------- */
+/* ----------  events  ----------- */
+/* ----------------------------------- */
 
 .grid {
   padding-left: 100px;
@@ -97,6 +179,81 @@ h3 {
   object-fit: cover;
   border-radius: 16px;
   overflow: hidden;
+}
+
+/* ----------------------------------- */
+/* ----------  links-news  ----------- */
+/* ----------------------------------- */
+.links-news-container {
+  padding-left: calc(100px + 32px);
+  padding-right: 100px;
+}
+
+.link-news {
+  display: block;
+  text-decoration: none;
+  padding: 32px 0;
+
+  border-bottom: 2px dashed #dddfdd;
+}
+
+.link-news:first-child {
+  border-top: 2px dashed #dddfdd;
+}
+
+.link-news .date {
+  font-family: 'Poppins';
+  font-weight: 400;
+  font-size: 14px;
+  /* line-height: 100%; */
+  letter-spacing: 0%;
+  color: #7f7f7f;
+}
+
+.link-news .title {
+  font-family: 'Noto Sans';
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 100%;
+  letter-spacing: 0%;
+  color: #000000;
+
+  padding-top: 16px;
+}
+
+.link-news .title {
+  width: 80%;
+  /* 强制文本不换行 */
+  white-space: nowrap;
+  /* 隐藏溢出内容 */
+  overflow: hidden;
+  /* 显示省略号 */
+  text-overflow: ellipsis;
+  /* 非块级元素需要添加(如span标签) */
+  display: block;
+}
+
+.links-news-nav {
+  width: fit-content;
+  margin: 0 auto;
+  display: flex;
+  gap: 16px;
+  padding-top: 24px;
+}
+
+.circle {
+  width: 12px;
+  height: 12px;
+  background-color: #dddfdd;
+  border-radius: 50%;
+}
+
+.circle.selected {
+  background-color: #4e07d8;
+}
+
+.circle:hover {
+  transform: scale(1.5);
 }
 
 @media (min-width: 1024px) {
